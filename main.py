db220d122828097adfb512219e249288fe020871
mrbach <bachtiar.kurniawan@gmail.com>
2019-11-17 14:09:50 +0700

    sintaks dasar

---
+++ main.py
@@ -0,0 +1,25 @@
+print('hi...pythonity')
+
+# menghitung luas
+
+alas = 10
+tinggi = 5
+luas = alas * tinggi
+
+print('Luas persegi dengan alas', alas, 'dan tinggi', tinggi, 'adalah ', luas)
+
+# logika percabangan
+print('='*25)
+nilai = 60
+
+if(nilai < 70):
+    print('nilai kamu C')
+elif(nilai == 80):
+    print('nilai kamu B')
+else:
+    print('nilai kamu A')
+
+# pengulangan
+
+for i in range(0,5):
+    print(i+1, luas)
